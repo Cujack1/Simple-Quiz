270fd0c68772f2df54c180432d9bbc5a5bed887b
Cujack <cujackxmanxmythxlegend@gmail.com>
2022-09-02 20:05:23 -0500

    fixed bug with populating answers

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Buttons
 const startBtn = document.querySelector(".start-btn");
+startBtn.addEventListener("click", start);
+
 const nextBtn = document.querySelector(".quiz-body__next");
+nextBtn.addEventListener("click", next);
+
 const previousBtn = document.querySelector(".quiz-body__previous");
+previousBtn.addEventListener("click", previous);
 
 // Question and Answer
 const questions = document.getElementById("question");
@@ -18,12 +23,7 @@ const questionDatabase = [
     id: 0,
     $question:
       "Guitar Anatomy: Where on the guitar do you place your hands to play notes?",
-    answers: [
-      { true: "Neck/Fretboard" },
-      { false: "Body" },
-      { false: "The Cutaway" },
-      { false: "Anywhere" },
-    ],
+    answers: ["Neck/Fretboard", "Body", "The Cutaway", "Anywhere"],
     message:
       "While it is true that you can hit or play anywhere on the guitar to make sounds, the neck is where the primary section of the guitar for producing notes. Sometimes you are using the guitar as a percussive instruemnt, sometimes you are plucking other sections of the string to produce interesting sounds. But the neck is the main place for playing notes.",
   },
@@ -31,10 +31,10 @@ const questionDatabase = [
     id: 1,
     $question: "What do most guitars use to adjust tuning on the strings?",
     answers: [
-      { true: "Tuning Knobs" },
-      { false: "Floating Bridge" },
-      { false: "String Bend" },
-      { false: "Adjust Action" },
+      "Tuning Knobs",
+      "Floating Bridge",
+      "String Bend",
+      "Adjust Action",
     ],
     message:
       "While most floating bridges do have 'fine-tuners' on them, most guitars do not have floating bridges. Most guitars just use the tuning knobs on the headstock to adjust to whatever tuning the player wants. For guitars with floating bridges: These guitars usually also have tuning locks at the end of the fretboard, right before the headstock, that prevent the guitar from coming out of tune. Therefore, any small tuning issue due to your strings stretching can be fixed with the fine-tuners on the bridge, versus a normal guitar which goes out of tune because a string moves around in the tuning knob. Tuning locks prevent a string from moving around in the tuning knob.",
@@ -42,7 +42,7 @@ const questionDatabase = [
   {
     id: 100,
     $question: "",
-    answers: [{ true: "" }, { false: "" }, { false: "" }, { false: "" }],
+    answers: ["", "", "", ""],
     message: "",
   },
 ];
@@ -60,12 +60,10 @@ let randomID;
 let chosenQuestion;
 let selections;
 let selectionsLength;
-const clickable = document.querySelectorAll(".clickable");
 
 // =============================================================================================
 
 // Initialization of the Quiz
-startBtn.addEventListener("click", start);
 function start() {
   // 1. Hide the start button to signify the beginning of the Quiz
   startBtn.classList.add("fade-away");
@@ -117,51 +115,42 @@ function populateQuestionAndAnswerHTML() {
   console.log("@ '0': ", answerOrder[0]);
 
   // Populate answers in HTML as unordered list.
-  for (key of selections) {
-    console.log(chosenQuestion.answers[0]);
+  console.log(selections[select1]);
 
-    answer1.innerHTML = selections[select1];
-    answer1.addEventListener("click", checkAnswer);
-    answer1.classList.add("clickable");
+  answer1.innerHTML = selections[select1];
+  answer1.addEventListener("click", checkAnswer);
+  answer1.classList.add("clickable");
 
-    answer2.innerHTML = selections[select2];
-    answer2.addEventListener("click", checkAnswer);
-    answer2.classList.add("clickable");
+  answer2.innerHTML = selections[select2];
+  answer2.addEventListener("click", checkAnswer);
+  answer2.classList.add("clickable");
 
-    answer3.innerHTML = selections[select3];
-    answer3.addEventListener("click", checkAnswer);
-    answer3.classList.add("clickable");
+  answer3.innerHTML = selections[select3];
+  answer3.addEventListener("click", checkAnswer);
+  answer3.classList.add("clickable");
 
-    answer4.innerHTML = selections[select4];
-    answer4.addEventListener("click", checkAnswer);
-    answer4.classList.add("clickable");
-  }
+  answer4.innerHTML = selections[select4];
+  answer4.addEventListener("click", checkAnswer);
+  answer4.classList.add("clickable");
 }
 
 function checkAnswer() {
   // Remove event listeners and clickability from list items
+  const clickable = document.querySelectorAll(".clickable");
   clickable.forEach((li) => {
     li.removeEventListener("click", checkAnswer);
     li.classList.remove("clickable");
   });
 
   // Check if the selected answer is correct - display message accordingly
-  for (key in selections[0]) {
-    if (this.innerHTML === selections[0][key]) {
-      console.log("good Job!");
-      this.classList.add("correct");
-    } else {
-      console.log(chosenQuestion.message);
-      this.classList.add("incorrect");
-    }
+  if (this.innerHTML === selections[0]) {
+    console.log("good Job!");
+    this.classList.add("correct");
+  } else {
+    console.log(chosenQuestion.message);
+    this.classList.add("incorrect");
   }
   nextOrPrevious();
-}
-
-function nextOrPrevious() {
-  console.log("next previous is called");
-  nextBtn.addEventListener("click", next);
-  previousBtn.addEventListener("click", previous);
 }
 
 function next() {
